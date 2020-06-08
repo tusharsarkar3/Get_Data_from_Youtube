401cc83cbad0aaaffb0e66a89069a78b5c222dca
tusharsarkar3 <59759357+tusharsarkar3@users.noreply.github.com>
2020-06-09 00:55:11 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,43 +1,28 @@
-# -*- coding: utf-8 -*-
-
 from apiclient.discovery import build
-# from apiclient.errors import HttpError
-# from oauth2client.tools import argparser # removed by Dongho
 import argparse
 import csv
 import unidecode
-
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyAsxL5dXwn-7jPWILKzzoTplZ4Lb9uh6SI"
+DEVELOPER_KEY = "" #Enter your API key here
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(options):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-    # Call the search.list method to retrieve results matching the specified
-    # query term.
     search_response = youtube.search().list(q=options.q, part="id,snippet", maxResults=options.max_results).execute()
 
     videos = []
     channels = []
     playlists = []
 
-    # create a CSV output for video list
     csvFile = open('video_result.csv', 'w')
     csvWriter = csv.writer(csvFile)
     csvWriter.writerow(["title", "videoId", "viewCount", "likeCount", "dislikeCount", "commentCount", "favoriteCount"])
 
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
     for search_result in search_response.get("items", []):
         if search_result["id"]["kind"] == "youtube#video":
-            # videos.append("%s (%s)" % (search_result["snippet"]["title"],search_result["id"]["videoId"]))
             title = search_result["snippet"]["title"]
-            title = unidecode.unidecode(title)  # Dongho 08/10/16
+            title = unidecode.unidecode(title)  
             videoId = search_result["id"]["videoId"]
             video_response = youtube.videos().list(id=videoId, part="statistics").execute()
             for video_result in video_response.get("items", []):
@@ -69,7 +54,4 @@ if __name__ == "__main__":
     parser.add_argument("--q", help="Search term", default="Google")
     parser.add_argument("--max-results", help="Max results", default=25)
     args = parser.parse_args()
-    # try:
     youtube_search(args)
-    # except HttpError, e:
-    #    print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
